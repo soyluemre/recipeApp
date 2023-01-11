f334664afa72c95e813a66a2614a206919365e5b
soyluemre <emresoyludev@gmail.com>
2023-01-11 21:38:43 +0300

    add login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,40 @@
 import React from "react";
-import { LoginContainer } from "./Login.style";
+import {
+  LoginContainer,
+  FromContainer,
+  StyledImg,
+  StyledInput,
+  StyledButton,
+  StyledForm,
+} from "./Login.style";
+import meal from "../../assets/meal.svg";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <LoginContainer></LoginContainer>;
+  const navigate = useNavigate();
+
+  const userInfo = {
+    email: "emre@mail.com",
+    password: "12345",
+  };
+
+  const handleSubmit = (e) => {
+    sessionStorage.setItem("user", JSON.stringify(userInfo));
+    navigate(-1);
+    e.preventDefault();
+  };
+  return (
+    <LoginContainer>
+      <FromContainer>
+        <StyledImg src={meal} />
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput placeholder="E-Mail" type="email" />
+          <StyledInput placeholder="Password" type="password" />
+          <StyledButton type="submit">Login</StyledButton>
+        </StyledForm>
+      </FromContainer>
+    </LoginContainer>
+  );
 };
 
 export default Login;
--- src/pages/login/Login.style.jsx
+++ src/pages/login/Login.style.jsx
@@ -3,8 +3,68 @@ import Flex from "../../components/globalStyles/Flex.style";
 
 export const LoginContainer = styled(Flex)`
   height: 100vh;
-  background-image: url("https://picsum.photos/1600/900");
+  background-image: url("https://img.rawpixel.com/private/static/images/website/2022-05/px1369813-image-kwvxxp91.jpg?w=1200&h=1200&dpr=1&fit=clip&crop=default&fm=jpg&q=75&vib=3&con=3&usm=15&cs=srgb&bg=F4F4F3&ixlib=js-2.2.1&s=b0bb6d55ae739cf84787c39dfe80d0ba");
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
+`;
+
+export const FromContainer = styled(Flex)`
+  width: 40rem;
+  height: 40rem;
+  max-width: 50rem;
+  background: rgba(198, 198, 198, 0.5);
+  border-radius: 40%;
+  padding: 0.5rem;
+  flex-direction: column;
+`;
+
+export const Header = styled.h1`
+  color: white;
+  font-size: 2rem;
+`;
+
+export const StyledInput = styled.input`
+  height: 3rem;
+  font-size: 1.4rem;
+  width: 28rem;
+  border-radius: 5px;
+  background: rgba(11, 11, 11, 0.6);
+  border: none;
+  color: white;
+  margin: 1rem;
+  text-indent: 1rem;
+  &:focus {
+    background: rgba(112, 7, 7, 0.716);
+    border: 1px solid red;
+    transition: all 1s;
+    border: none;
+  }
+`;
+
+export const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const StyledButton = styled.button`
+  font-size: 1.4rem;
+  background: rgba(112, 7, 7, 0.716);
+  border-radius: 5px;
+  color: white;
+  margin: 1rem;
+  border: none;
+  padding: 0.5rem;
+  cursor: pointer;
+  &:hover {
+    background: rgba(11, 11, 11, 0.6);
+    transition: box-shadow 0.7s;
+    transition: all 0.7s;
+    color: red;
+  }
+`;
+
+export const StyledImg = styled.img`
+  width: 200px;
+  margin: 1rem;
 `;
