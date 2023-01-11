2e933a3ae41c3165b622c0f9e95de19eba8f3d66
soyluemre <emresoyludev@gmail.com>
2023-01-11 19:30:49 +0300

    start project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import AppRouter from "../src/router/AppRouter";
+
+function App() {
+  return (
+    <div className="App">
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { GlobalStyles } from "../components/globalStyles/GlobalStyles";
+import Navbar from "./../components/nav/Navbar";
+import Home from "../pages/home/Home";
+import Login from "../pages/login/Login";
+import About from "../pages/about/About";
+import Detail from "../pages/detail/Detail";
+import PrivateRouter from "./PrivateRouter";
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <GlobalStyles />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="/about" element={<PrivateRouter />}>
+          <Route path="" element={<About />} />
+        </Route>
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
